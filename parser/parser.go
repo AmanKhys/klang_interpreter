e6a87eaf93aaaef35b01d3ec53c7d1fec8841eff
amankhys <amankhys2112@gmail.com>
2026-06-03 13:39:46 +0530

    write parsing function and testing for prefix operators; write parsing function and testing for integer literal that can be used in expression parsing, used here for prefix operators and to be used for later expression parsing;

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"pi/ast"
 	"pi/lexer"
 	"pi/token"
+	"strconv"
 )
 
 const (
@@ -44,6 +45,9 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.PrefixParseFns = make(map[token.TokenType]PrefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
 	return p
 }
@@ -155,15 +159,43 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.PrefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
 	return leftExp
 }
 
+func (p *Parser) parsePrefixExpression() *ast.PrefixExpression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+	p.nextToken()
+	expression.Right = p.parseExpression(PREFIX)
+	return expression
+}
+
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
